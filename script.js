ed77770ad6c18795d596b0830c77b3c73cc2b050
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-21 23:57:15 -0600

    Seconds and Minutes JS Complete

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 
 
-function getSecondsSinceStartOfDay() {
-    return new Date().getSeconds() + 
+
+
+
+
+
+function getSecond() {
+    return new Date().getSeconds()/* + 
       new Date().getMinutes() * 60 + 
+      new Date().getHours() * 3600;*/
+}
+
+function getMinute() {
+    return new Date().getMinutes()
+}
+
+function getHour() {
+    return /*new Date().getSeconds() + 
+      new Date().getMinutes() * 60 + */
       new Date().getHours() * 3600;
 }
 
+
 setInterval( function() {
-    var time = getSecondsSinceStartOfDay();
-    console.log(time);
+    var timeSecond = getSecond();
+    console.log(timeSecond);
+    var secondDegree = timeSecond * 6 + 180;
+    console.log(secondDegree);
+    var second = document.querySelector("#seconds");
+    second.style.transform = "rotate(" + secondDegree + "deg)";
+    console.log(second.style.transform);
+
+    var timeMinute = getMinute();
+    console.log(timeMinute);
+    var minuteDegree = timeMinute * 6 + 180;
+    console.log(minuteDegree); 
+    var minute = document.querySelector("#minutes");
+    minute.style.transform = "rotate(" + minuteDegree + "deg)";
+    console.log(minute.style.transform);
+
+    
     // your code here
 }, 1000);
